1c69e992356441af3fbcb2f8f2732b3c8fe87520
boterosalas <boterosalas@gmail.com>
2024-10-15 14:49:41 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const port = 8080;
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html')
+});
+
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html')
+});
+
+app.get('/*', function (req, res) {
+    res.send('404 | Page not found');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
